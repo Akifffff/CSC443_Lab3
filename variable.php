d6e38296e87b9d94ec7e86f0c266b2a87e7baeb4
Akifffff <abdakif12@gmail.com>
2022-04-22 00:05:00 +0800

    variable commit

---
+++ variable.php
@@ -0,0 +1,14 @@
+<html>
+    <body>
+        <?php
+        $username = "Fred Smith"
+        echo $username;
+        echo "<br />";
+        $current_user = $username;
+        echo $current_user;
+        echo "<br />";
+        $numl = 10.5;
+        echo $num1;
+        ?>
+    </body>
+</html>
